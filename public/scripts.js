c224f4b17b5f792f71f589e45b2b20f7c6a4921a
Juan Rodriguez <devjuanr997@gmail.com>
2025-01-13 17:18:21 -0300

    Quité el desplazamiento automático

--- public/scripts.js
+++ public/scripts.js
@@ -6,12 +6,14 @@ const updateButtonsVisibility = () => {
     const scrollPosition = window.scrollY;
     const windowHeight = window.innerHeight;
 
+    // Mostrar el botón "scroll-up" si está suficientemente abajo
     if (scrollPosition > 200) {
         scrollUpButton.style.display = "block";
     } else {
         scrollUpButton.style.display = "none";
     }
 
+    // Mostrar solo el botón de "scroll-down" para la sección actual
     sections.forEach((section, index) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.offsetHeight;
@@ -24,60 +26,28 @@ const updateButtonsVisibility = () => {
     });
 };
 
-const scrollToNearestSection = () => {
-    const isResponsiveView = window.innerWidth <= 768;
-
-    if (isResponsiveView) return;
-
-    const scrollPosition = window.scrollY;
-    const windowHeight = window.innerHeight;
-
-    let nearestSection = sections[0];
-    let minDistance = Infinity;
-
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-        const sectionCenter = sectionTop + sectionHeight / 2;
-        const distance = Math.abs(sectionCenter - (scrollPosition + windowHeight / 2));
-
-        if (distance < minDistance) {
-            minDistance = distance;
-            nearestSection = section;
-        }
-    });
-
-    nearestSection.scrollIntoView({ behavior: "smooth" });
-};
-
-let debounceTimeout;
-window.addEventListener("scroll", () => {
-    clearTimeout(debounceTimeout);
-
-    debounceTimeout = setTimeout(() => {
-        updateButtonsVisibility();
-        scrollToNearestSection();
-    }, 100);
-});
-
-window.addEventListener("load", () => {
-    updateButtonsVisibility();
-});
-
+// Desplazarse hacia abajo al hacer clic en un botón
 scrollDownButtons.forEach((button, index) => {
-    console.log("Boton presionado");
-    
     button.addEventListener("click", () => {
-        const nextSection = sections[index + 1];
-        console.log("Siguiente sección ", nextSection);
+        const nextSection = sections[index + 1]; // Encuentra la siguiente sección
         if (nextSection) {
             nextSection.scrollIntoView({ behavior: "smooth" });
         }
     });
 });
 
+// Desplazarse hacia arriba al hacer clic en el botón "scroll-up"
 scrollUpButton.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// Actualizar los botones de visibilidad al cargar la página y al desplazarse
+window.addEventListener("load", () => {
+    updateButtonsVisibility();
+});
+
+window.addEventListener("scroll", () => {
+    updateButtonsVisibility();
 });
 
 const gradient = document.getElementById("gradient");
